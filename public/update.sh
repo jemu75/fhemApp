2795bca9937bfc36c49bbd4bf233e0773c545cf5
jemu75 <jens@jateam.de>
2021-05-14 13:49:54 +0200

    v3.20.2

---
+++ public/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+gitrepo=https://github.com/jemu75/fhemApp.git
+tempdir=update
+appdir=/opt/fhem/www
+echo ---------------------------------------------------------------
+echo $(date) " Starting Update from FHEMApp..."
+echo ---------------------------------------------------------------
+rm -rf $tempdir
+git clone $gitrepo $tempdir
+rsync -av $tempdir/www/fhemapp $appdir --exclude cfg | sed '0,/^$/d'
+rm -rf $tempdir
+echo ---------------------------------------------------------------
+echo $(date) " Updating FHEMApp is finished."
+echo ---------------------------------------------------------------
