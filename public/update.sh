6824ce00de8275557698d095e2861e928552e3f4
jens <jens@jateam.de>
2022-03-06 12:39:36 +0100

    update bash script

--- public/update.sh
+++ public/update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 gitrepo=https://github.com/jemu75/fhemApp.git
 tempdir=update
-appdir=/opt/fhem/www
+appdir=$(dirname "$(readlink -e "$0")")
 echo ---------------------------------------------------------------
 echo $(date) " Starting Update FHEMApp..."
 echo ---------------------------------------------------------------
 rm -rf $tempdir
 git clone $gitrepo $tempdir
-rsync -av $tempdir/www/fhemapp $appdir --exclude cfg | sed '0,/^$/d'
+rsync -av $tempdir/www/fhemapp/ $appdir --exclude cfg | sed '0,/^$/d'
 rm -rf $tempdir
 echo ---------------------------------------------------------------
 echo $(date) " Updating FHEMApp is finished."
