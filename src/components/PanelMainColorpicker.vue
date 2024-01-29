fafdd4c5997822bf452cdd6cb27243661ea13471
jemu75 <jens@jateam.de>
2024-01-29 22:51:28 +0100

    v4.0.14-beta

--- src/components/PanelMainColorpicker.vue
+++ src/components/PanelMainColorpicker.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { computed } from 'vue'
     import { useFhemStore } from '@/stores/fhem'
     import ColorPicker from '@radial-color-picker/vue-color-picker'
 
@@ -10,11 +11,83 @@
     })
 
     const fhem = useFhemStore()
+
+    const color = computed(() => {
+        return fhem.handleDefs(props.el.picker, ['cmd', 'current'],['', false])
+    })
+
+    function doCmd(val) {
+        let hex = hslToHex(val, 50, 100),
+            cmd = color.value.cmd,
+            defParts = []
+
+        cmd = cmd.replace('%v', hex)
+
+        for(const device of props.devices) {
+            defParts = device.split(':')
+            if(RegExp(defParts[0]).test(cmd)) cmd = cmd.replace(defParts[0], defParts[1])
+        }
+
+        //console.log(cmd)
+        fhem.request('text', cmd)
+    }
+
+    function hslToHex(h, l, s) {
+        l /= 100
+        const a = s * Math.min(l, 1 - l) / 100
+        const f = n => {
+            const k = (n + h / 30) % 12
+            const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
+            return Math.round(255 * color).toString(16).padStart(2, '0')   // convert to Hex and prefix "0" if needed
+        }
+
+        return `${f(0)}${f(8)}${f(4)}`
+    }
+
+    function hexToHSL(hex) {
+        hex = hex.replace(/^#/, '')
+
+        let bigint = parseInt(hex, 16),
+            r = (bigint >> 16) & 255,
+            g = (bigint >> 8) & 255,
+            b = bigint & 255
+
+        r /= 255
+        g /= 255
+        b /= 255
+
+        let max = Math.max(r, g, b),
+            min = Math.min(r, g, b),
+            delta = max - min
+
+        let l = (max + min) / 2,
+            h = 0,
+            s = 0
+
+        if (delta !== 0) {
+            if (max === r) {
+                h = ((g - b) / delta + (g < b ? 6 : 0)) % 6
+            } else if (max === g) {
+                h = ((b - r) / delta + 2) % 6
+            } else {
+                h = ((r - g) / delta + 4) % 6
+            }
+
+            h = h * 60
+
+            s = delta / (1 - Math.abs(2 * l - 1))
+        }
+
+        s = Math.round(s * 100)
+        l = Math.round(l * 100)
+
+        return { hue: h, saturation: s, luminosity: l }
+    }
 </script>
 
 <template>
-    <div class="ma-2">
-        <ColorPicker :initially-collapsed="true"></ColorPicker>
+    <div class="mt-4 mb-2">
+        <ColorPicker v-bind="hexToHSL(color.current)" variant="persistent" @change="doCmd($event)"></ColorPicker>
     </div>
 </template>
 
--- www/fhemapp4/assets/InternalsView-00727e6e.js
+++ www/fhemapp4/assets/InternalsView-00727e6e.js
@@ -1,4 +1,4 @@
-import{c as Z,g as q,V as tt}from"./styles-c2b92407.js";import{f as et,u as nt,c as ot,r as E,o as rt,a as it,w as S,e as w,j as V,t as F,p as at,h as ut}from"./index-61acc08e.js";var I={exports:{}};/*!
+import{c as Z,g as q,V as tt}from"./styles-7766865e.js";import{f as et,u as nt,c as ot,r as E,o as rt,a as it,w as S,e as w,j as V,t as F,p as at,h as ut}from"./index-c55da4ce.js";var I={exports:{}};/*!
  * clipboard.js v2.0.11
  * https://clipboardjs.com/
  *
